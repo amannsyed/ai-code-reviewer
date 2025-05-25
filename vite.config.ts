840aa4d3684b000f9152b2cafd90e2f493f5aa15
amannsyed <amansyed3342@gmail.com>
2025-05-25 02:38:58 +0100

    build update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/ai-code-reviewer/",
+    base: "/ai-code-reviewer",
     plugins: [react()], // Add this
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
